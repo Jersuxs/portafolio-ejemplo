51d758004638afda803970b567b44c930263bebc
Jersuxs <116213804+Jersuxs@users.noreply.github.com>
2024-11-26 22:11:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-// Aquí puedes agregar código JavaScript para agregar interactividad a tu portafolio
+/* Importar variables de configuración */
+    @import 'config.css';
+
+    /* Estilos generales */
+    body {
+      font-family: $font-family;
+      margin: 0;
+      padding: 0;
+      background-color: $background-color;
+    }
+
+    header {
+      background-color: $header-background-color;
+      color: $header-color;
+      padding: 1em;
+      text-align: center;
+    }
+
+    nav ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    nav li {
+      margin-right: 20px;
+    }
+
+    nav a {
+      color: $header-color;
+      text-decoration: none;
+    }
+
+    main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 2em;
+    }
+
+    section {
+      background-color: $section-background-color;
+      padding: 1em;
+      margin-bottom: 20px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    h1, h2, h3 {
+      color: $title-color;
+    }
+
+    p {
+      color: $text-color;
+    }
+
+    a {
+      color: $link-color;
+      text-decoration: none;
+    }
+
+    a:hover {
+      color: $link-hover-color;
+    }
